d3c65998e02c2c18c207e0d2f529cf3b710279dc
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-12 22:31:52 +0200

    Initial commit
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

---
+++ app/src/main/java/fi/tuni/androidsudoku/sudoku/Generator.java
@@ -0,0 +1,61 @@
+package fi.tuni.androidsudoku.sudoku;
+
+/**
+ *
+ */
+public class Generator {
+
+    /**
+     *
+     */
+    private Cell[] puzzle;
+
+    /**
+     *
+     */
+    public Generator() {
+        puzzle = new Cell[Constants.GRID_SIZE];
+
+        for (int i = 0; i < puzzle.length; i++) {
+            puzzle[i] = new Cell(i);
+        }
+    }
+
+    /**
+     *
+     * @param index
+     * @return
+     */
+    private boolean isInRange(int index) {
+        return index >= 0 && index < puzzle.length;
+    }
+
+    /**
+     *
+     * @param index
+     * @param value
+     * @return
+     */
+    private boolean setCell(int index, int value) {
+        if (isInRange(index)) {
+            Cell cell = puzzle[index];
+
+            if (!cell.isLocked()) {
+                return cell.setValue(value);
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param value
+     * @return
+     */
+    private boolean setCell(int x, int y, int value) {
+        return setCell(x * Constants.GRID_SET + y, value);
+    }
+}
