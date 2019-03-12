eb43be14e76c47f87bb1bfe804f5a6b0cf6db40f
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-12 22:20:38 +0200

    Initial commit
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

---
+++ app/src/main/java/fi/tuni/androidsudoku/sudoku/Constants.java
@@ -0,0 +1,21 @@
+package fi.tuni.androidsudoku.sudoku;
+
+/**
+ *
+ */
+public final class Constants {
+
+    public static final int GRID = 3;
+    public static final int GRID_SET = GRID * GRID;
+    public static final int GRID_SIZE = GRID_SET * GRID_SET;
+
+    public static final int MIN_CELL_VALUE = 1;
+    public static final int MAX_CELL_VALUE = 9;
+
+    /**
+     *
+     */
+    private Constants() {
+        // Deny instantiation
+    }
+}
