113ca4cdb9470e4cf36472b18156c01af33dc204
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-21 00:42:40 +0200

    Initial commit
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

---
+++ app/src/main/java/fi/tuni/androidsudoku/PuzzleActivity.java
@@ -0,0 +1,13 @@
+package fi.tuni.androidsudoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PuzzleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle);
+    }
+}
