4063b6d6f93cc421e9859cce9b94d60c0300ed24
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-21 00:46:32 +0200

    Constants: Add more constant values
    - Add JavaDoc template to existing constants
    
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/sudoku/Constants.java
+++ app/src/main/java/fi/tuni/androidsudoku/sudoku/Constants.java
@@ -5,12 +5,42 @@ package fi.tuni.androidsudoku.sudoku;
  */
 public final class Constants {
 
+    /**
+     *
+     */
     public static final int GRID = 3;
+
+    /**
+     *
+     */
     public static final int GRID_SET = GRID * GRID;
+
+    /**
+     *
+     */
     public static final int GRID_SIZE = GRID_SET * GRID_SET;
 
+    /**
+     *
+     */
     public static final int MIN_CELL_VALUE = 1;
+
+    /**
+     *
+     */
     public static final int MAX_CELL_VALUE = 9;
+
+    /**
+     *
+     */
+    public static final int EMPTY_CELL_VALUE = 0;
+
+    /**
+     *
+     */
+    public static final int[] ALLOWED_VALUES = {
+        1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
 
     /**
      *
