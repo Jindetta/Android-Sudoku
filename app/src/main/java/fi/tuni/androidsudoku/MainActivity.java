adda95fdbd1b590fd090cb0970dd4afd0048a235
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-24 04:14:02 +0200

    MainActivity: Apply animation to slideshow
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/MainActivity.java
+++ app/src/main/java/fi/tuni/androidsudoku/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = findViewById(R.id.image);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+        image.setAnimation(animation);
+        animation.start();
     }
 
     public void clickHandler(View view) {
