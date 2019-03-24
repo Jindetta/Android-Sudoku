9cfb20d8bf8eaac0156c10dd7bb99320a08a0d53
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-24 03:39:24 +0200

    TimerService: Add service for time management
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

---
+++ app/src/main/java/fi/tuni/androidsudoku/TimerService.java
@@ -0,0 +1,68 @@
+package fi.tuni.androidsudoku;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+
+/**
+ *
+ */
+public class TimerService extends Service {
+
+    /**
+     *
+     */
+    public static final String TIMER_EVENT = "UPDATE_TIMER";
+
+    /**
+     *
+     */
+    private long startTimestamp;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            startTimestamp = intent.getLongExtra("started", 0);
+
+            new Thread(this::updateEvent).start();
+            return START_STICKY;
+        }
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        startTimestamp = 0;
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /**
+     *
+     */
+    private void updateEvent() {
+        Intent event = new Intent(TIMER_EVENT);
+        LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
+
+        while (startTimestamp != 0) {
+            long time = System.currentTimeMillis() - startTimestamp;
+
+            event.putExtra("minutes", time / 1000 / 60);
+            event.putExtra("seconds", time / 1000 % 60);
+            event.putExtra("millis", time % 1000 / 10);
+            manager.sendBroadcast(event);
+
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                // Ignored
+            }
+        }
+    }
+}
