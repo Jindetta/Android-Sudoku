5d7a558c29fdae546b004b54d7f87a57e9b5ae7b
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-07 22:58:04 +0300

    SudokuPuzzle: Implement getListOfFilledCells()
    - Change generatePuzzle() implementation
    - Add JavaDoc dummies
    
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/sudoku/SudokuPuzzle.java
+++ app/src/main/java/fi/tuni/androidsudoku/sudoku/SudokuPuzzle.java
@@ -36,6 +36,11 @@ public class SudokuPuzzle {
         generatePuzzle(difficulty);
     }
 
+    /**
+     *
+     * @param puzzle
+     * @return
+     */
     private List<Cell> getListOfEmptyCells(Cell[] puzzle) {
         List<Cell> results = new ArrayList<>();
 
@@ -48,24 +53,43 @@ public class SudokuPuzzle {
         return results;
     }
 
+    /**
+     *
+     * @param puzzle
+     * @return
+     */
+    private List<Cell> getListOfFilledCells(Cell[] puzzle) {
+        List<Cell> results = new ArrayList<>(puzzle.length);
+
+        for (Cell cell : puzzle) {
+            if (!cell.isEmpty()) {
+                results.add(cell);
+            }
+        }
+
+        return results;
+    }
+
+    /**
+     *
+     * @param difficulty
+     */
     private void generatePuzzle(Difficulty difficulty) {
-        int cellsLeft = puzzle.length - difficulty.getCluesCount();
+        List<Cell> filledCells = getListOfFilledCells(puzzle);
         Random random = new Random();
 
-        while (cellsLeft > 0) {
-            int index = random.nextInt(puzzle.length);
-            Cell randomCell = puzzle[index];
+        while (filledCells.size() > difficulty.getCluesCount()) {
+            int index = random.nextInt(filledCells.size());
+            Cell randomCell = filledCells.get(index);
 
-            if (!randomCell.isEmpty()) {
-                int value = randomCell.getValue();
-                randomCell.setEmpty();
+            int value = randomCell.getValue();
+            randomCell.setEmpty();
 
-                if (randomCell.hasSingleValidValue() || isUniquePuzzle()) {
-                    randomCell.setLocked(false);
-                    cellsLeft--;
-                } else {
-                    randomCell.setValue(value);
-                }
+            if (randomCell.hasSingleValidValue() || isUniquePuzzle()) {
+                randomCell.setLocked(false);
+                filledCells.remove(index);
+            } else {
+                randomCell.setValue(value);
             }
         }
     }
@@ -107,6 +131,11 @@ public class SudokuPuzzle {
         return verifyPuzzle(puzzle);
     }
 
+    /**
+     *
+     * @param puzzle
+     * @return
+     */
     private boolean verifyPuzzle(Cell[] puzzle) {
         for (int i = 0; i < puzzle.length; i++) {
             if (puzzle[i].getValue() != solution.get(i).getValue()) {
@@ -163,29 +192,50 @@ public class SudokuPuzzle {
         return builder.toString();
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return getPuzzleString(null);
     }
 
+    /**
+     *
+     */
     public enum Difficulty {
         NONE        (81),
         EASY        (44),
         MEDIUM      (39),
         HARD        (36),
-        VERY_HARD   (27);
+        VERY_HARD   (28);
 
+        /**
+         *
+         */
         private int cluesCount;
 
+        /**
+         *
+         * @param cluesCount
+         */
         Difficulty(int cluesCount) {
             this.cluesCount = cluesCount;
         }
 
+        /**
+         *
+         * @return
+         */
         public int getCluesCount() {
             return cluesCount;
         }
     }
 
+    /**
+     *
+     */
     public enum Format {
         USE_DOT_FILL            ('.'),
         USE_ZERO_FILL           ('0'),
@@ -193,12 +243,23 @@ public class SudokuPuzzle {
         USE_HASH_TAG_FILL       ('#'),
         USE_QUESTION_MARK_FILL  ('?');
 
+        /**
+         *
+         */
         private char fillCharacter;
 
+        /**
+         *
+         * @param fillCharacter
+         */
         Format(char fillCharacter) {
             this.fillCharacter = fillCharacter;
         }
 
+        /**
+         *
+         * @return
+         */
         public char getFillCharacter() {
             return fillCharacter;
         }
