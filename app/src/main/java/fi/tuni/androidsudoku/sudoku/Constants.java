1f3273c439ed6768abf1040cf9bed7c761b02928
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-19 23:25:12 +0300

    Constants: Add JavaDoc
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/sudoku/Constants.java
+++ app/src/main/java/fi/tuni/androidsudoku/sudoku/Constants.java
@@ -1,54 +1,54 @@
 package fi.tuni.androidsudoku.sudoku;
 
 /**
- *
+ * Class containing all important constants needed for puzzle generation.
  */
 public final class Constants {
 
     /**
-     *
+     * Defines grid multiplier.
      */
     public static final int MULTIPLIER = 3;
 
     /**
-     *
+     * Defines grid group size.
      */
     public static final int GROUP_SIZE = MULTIPLIER * MULTIPLIER;
 
     /**
-     *
+     * Defines puzzle size.
      */
     public static final int PUZZLE_SIZE = GROUP_SIZE * GROUP_SIZE;
 
     /**
-     *
+     * Defines puzzle verification value.
      */
     public static final int PUZZLE_VERIFY_VALUE = 511;
 
     /**
-     *
+     * Defines minimum allowed value.
      */
     public static final int MIN_CELL_VALUE = 1;
 
     /**
-     *
+     * Defines maximum allowed value.
      */
     public static final int MAX_CELL_VALUE = 9;
 
     /**
-     *
+     * Defines empty cell value.
      */
     public static final int EMPTY_CELL_VALUE = 0;
 
     /**
-     *
+     * Defines all allowed values.
      */
     public static final int[] ALLOWED_VALUES = {
         1, 2, 3, 4, 5, 6, 7, 8, 9
     };
 
     /**
-     *
+     * Overrides default constructor.
      */
     private Constants() {
         // Deny instantiation
