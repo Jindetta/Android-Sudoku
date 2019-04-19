76cd6f127a6d3e100bab4a4b14685b0657e252d9
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-19 23:21:47 +0300

    TimerService: Add JavaDoc
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/TimerService.java
+++ app/src/main/java/fi/tuni/androidsudoku/TimerService.java
@@ -6,20 +6,28 @@ import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 
 /**
- *
+ * Service for keeping track of time.
  */
 public class TimerService extends Service {
 
     /**
-     *
+     * Defines event identifier.
      */
     public static final String TIMER_EVENT = "UPDATE_TIMER";
 
     /**
-     *
+     * Stores initial timestamp.
      */
     private volatile long startTimestamp;
 
+    /**
+     * Starts service.
+     *
+     * @param intent    Sender intent.
+     * @param flags     Sender flags.
+     * @param startId   Start identifier.
+     * @return          Service state value.
+     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null) {
@@ -32,19 +40,28 @@ public class TimerService extends Service {
         return START_NOT_STICKY;
     }
 
+    /**
+     * Destroys service.
+     */
     @Override
     public void onDestroy() {
         startTimestamp = -1;
         super.onDestroy();
     }
 
+    /**
+     * Binds service (not used).
+     *
+     * @param intent    Intent data.
+     * @return          null
+     */
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
     /**
-     *
+     * Updates timestamp.
      */
     private void updateEvent() {
         Intent event = new Intent(TIMER_EVENT);
