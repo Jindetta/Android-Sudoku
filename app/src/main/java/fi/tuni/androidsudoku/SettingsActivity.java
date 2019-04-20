6695e57a0171ad5250efc4fe52c3846eb7be1231
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-21 00:50:32 +0300

    SettingsActivity: Add JavaDoc
    - Update settings saving behavior
    
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/SettingsActivity.java
+++ app/src/main/java/fi/tuni/androidsudoku/SettingsActivity.java
@@ -2,33 +2,59 @@ package fi.tuni.androidsudoku;
 
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
 
+/**
+ * Activity displaying settings.
+ */
 public class SettingsActivity extends AppCompatActivity {
 
+    /**
+     * Defines settings key.
+     */
+    public static final String KEY = "AndroidSudokuPreferences";
+
+    /**
+     * Creates activity.
+     *
+     * @param savedInstanceState Instance state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SharedPreferences prefs = getSharedPreferences(KEY, MODE_PRIVATE);
+
+        CheckBox highlightCells = findViewById(R.id.toggleHighlight);
+        highlightCells.setChecked(prefs.getBoolean("highlight", true));
+
+        CheckBox displayHints = findViewById(R.id.toggleHints);
+        displayHints.setChecked(prefs.getBoolean("hints", true));
     }
 
+    /**
+     * Listens for click events.
+     *
+     * @param view Target view.
+     */
     public void settingsListener(View view) {
         if (view instanceof CheckBox) {
-            SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+            SharedPreferences.Editor prefs = getSharedPreferences(KEY, MODE_PRIVATE).edit();
             CheckBox setting = (CheckBox) view;
 
             switch (view.getId()) {
                 case R.id.toggleHighlight: {
-                    prefs.edit().putBoolean("highlight", setting.isChecked());
+                    prefs.putBoolean("highlight", setting.isChecked());
                 }
                 case R.id.toggleHints: {
-                    prefs.edit().putBoolean("hints", setting.isChecked());
+                    prefs.putBoolean("hints", setting.isChecked());
                 }
             }
 
-            prefs.edit().apply();
+            prefs.apply();
         }
     }
 }
