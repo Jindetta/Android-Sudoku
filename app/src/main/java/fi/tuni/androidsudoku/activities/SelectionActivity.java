50d58bc5aac82486ac35dedb58bf8f773106e460
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-22 22:53:01 +0300

    SelectionActivity: Change puzzleSelection() implementation
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- app/src/main/java/fi/tuni/androidsudoku/activities/SelectionActivity.java
+++ app/src/main/java/fi/tuni/androidsudoku/activities/SelectionActivity.java
@@ -36,27 +36,28 @@ public class SelectionActivity extends AppCompatActivity {
      * @param view Target view.
      */
     public void puzzleSelection(View view) {
+        final String DIFFICULTY_KEY = "difficulty";
         Intent activity = new Intent(this, PuzzleActivity.class);
 
         switch (view.getId()) {
             case R.id.easy: {
-                activity.putExtra("difficulty", Sudoku.Difficulty.EASY.name());
+                activity.putExtra(DIFFICULTY_KEY, Sudoku.Difficulty.EASY.name());
                 break;
             }
             case R.id.medium: {
-                activity.putExtra("difficulty", Sudoku.Difficulty.MEDIUM.name());
+                activity.putExtra(DIFFICULTY_KEY, Sudoku.Difficulty.MEDIUM.name());
                 break;
             }
             case R.id.hard: {
-                activity.putExtra("difficulty", Sudoku.Difficulty.HARD.name());
+                activity.putExtra(DIFFICULTY_KEY, Sudoku.Difficulty.HARD.name());
                 break;
             }
             case R.id.very_hard: {
-                activity.putExtra("difficulty", Sudoku.Difficulty.VERY_HARD.name());
+                activity.putExtra(DIFFICULTY_KEY, Sudoku.Difficulty.VERY_HARD.name());
                 break;
             }
             default: {
-                activity.putExtra("difficulty", Sudoku.Difficulty.NONE.name());
+                activity.putExtra(DIFFICULTY_KEY, Sudoku.Difficulty.NONE.name());
                 break;
             }
         }
